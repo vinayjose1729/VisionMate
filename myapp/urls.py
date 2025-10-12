74bfdde6aa21bb9284c5ac65c780c3840050a734
VJ <vinayjose2005@gmail.com>
2025-10-12 13:53:35 +0530

    Admin panel

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('',views.home,name ='home'),
     path('login',views.login,name ='login'),
     path('adminhome', views.adminhome, name='adminhome'),
-    path('caretaker', views.caretaker, name='caretaker')
+    path('caretaker', views.caretaker, name='caretaker'),
+    path('admin_view_blind_users',views.admin_view_blind_users,name='admin_view_blind_users'),
+    path('admin_view_complaint',views.admin_view_complaint,name='admin_view_complaint'),
+    path('admin_view_feedback', views.admin_view_feedback, name='admin_view_feedback'),
+    path('admin_complaint_reply', views.admin_complaint_reply, name='admin_complaint_reply')
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -13,3 +13,21 @@ def adminhome(request):
 
 def caretaker(request):
     return render(request,'adminpanel/verify.html')
+
+def admin_view_blind_users(request):
+    return render(request,'adminpanel/admin_view_blind_users.html')
+
+
+def admin_view_complaint(request):
+    return render(request,'adminpanel/admin_view_complaint.html')
+
+def admin_view_feedback(request):
+    return render(request,'adminpanel/admin_view_feedback.html')
+
+def admin_complaint_reply(request):
+    return render(request,'adminpanel/admin_complaint_reply.html')
+
+
+
+
+
